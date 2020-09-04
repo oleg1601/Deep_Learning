140f0b5b09b37bf04439a87522839b5dd30e4e05
oleg1601 <54068512+oleg1601@users.noreply.github.com>
2020-09-04 09:09:30 +0300

    Add files via upload

---
+++ articles/views.py
@@ -0,0 +1,43 @@
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from . import models
+
+# Create your views here.
+
+class ArticleListView(LoginRequiredMixin, ListView):
+    model = models.Article
+    template_name = 'article_list.html'
+    login_url = 'login'
+
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = models.Article
+    template_name = 'article_new.html'
+    fields = ['title', 'body']
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.indtance.author = self.request.user
+        return super().form_valid(form)
+
+
+class ArticleDetailView(LoginRequiredMixin, DetailView):
+    model = models.Article
+    template_name = 'article_detail.html'
+    login_url = 'login'
+
+
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
+    model = models.Article
+    fields = ['title', 'body', ]
+    template_name = 'article_edit.html'
+    login_url = 'login'
+
+
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
+    model = models.Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+    login_url = 'login'
